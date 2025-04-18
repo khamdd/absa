ad5557075f6c2b9459ffff31e659e59586fd4b06
KhamDang <dangduongkham1714@gmail.com>
2025-04-18 16:37:04 +1000

    khamdd_fix data issue

--- acsa_model.py
+++ acsa_model.py
@@ -15,7 +15,7 @@ class ABSA2025MultiTask(Model):
         self.last_4_hidden_states = Concatenate(name='last_4_hidden_states')
         self.dropout = Dropout(0.2)
         self.dense_layers = [
-            Dense(4, activation='softmax', name=label.replace('#', '-').replace('&', '_')) 
+            Dense(4, activation='softmax', name=label.replace('/', '-').replace('&', '_')) 
             for label in self.aspect_category_names
         ]
 
